678690ae9cae3484d21b69391899583d9102acb4
GiriDiwa98 <giridiwa571@gmail.com>
2023-10-30 21:49:43 +0700

    Update Sesi 1 Selesai

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -3,7 +3,13 @@ import Link from "next/link";
 const AnimeList = ({ title, images, id }) => {
   return (
     <Link href={`/${id}`} className="cursor-pointer">
-      <Image src={images} width={350} height={350} alt="" />
+      <Image
+        src={images}
+        width={350}
+        height={350}
+        alt=""
+        className="w-full max-h-64 object-cover"
+      />
       <h3 className="font-bold md:text-xl text-sm p-4"> {title} </h3>
     </Link>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import AnimeList from "./components/AnimeList";
 
 export default async function Home() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
   const anime = await response.json();
   return (
@@ -11,12 +12,9 @@ export default async function Home() {
         <h1 className="p-4 text-1xl  sm:text-2xl font-bold mx-1 ">
           List Anime Popular
         </h1>
-        <a
-          href="/"
-          className="py-2 px-2 sm:px-3 border bg-blue-400 border-blue-400 text-white text-1xl text-center rounded-full hover:ring-1 hover:ring-blue-500/20 hover:border-blue-500 hover:bg-blue-500 hover:ring-offset-1 shadow-sm"
-        >
+        <Link href="/populer" className="btn-see">
           Lihat Semua
-        </a>
+        </Link>
       </div>
       <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 mx-5 ">
         {anime.data.map((data) => {
